5a53e86d302d11997a5f9390158811881c25d9bf
Rob Paik <robpaik@gmail.com>
2019-06-28 02:32:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -76,4 +76,27 @@ def alan_kay_is_known_for
   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
   return programmer_hash
 end
+
+def changing_alan
+	  programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
+     
+end
   
