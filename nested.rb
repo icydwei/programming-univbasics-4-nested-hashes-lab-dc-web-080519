b8343062edfb91215d1390807902bcf09fff7ee5
Rob Paik <robpaik@gmail.com>
2019-06-28 02:34:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -99,4 +99,22 @@ def changing_alan
      return programmer_hash
      
 end
-  
+
+def adding_to_dennis
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+    programmer_hash[:dennis_ritchie][:languages].
+end
